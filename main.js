2db8fd57e0c88038ea9e8a34c512e03ca92bceb4
Ramazanovakrom0723 <akromramazanov77@gmail.com>
2024-04-01 08:59:23 +0500

    100%

--- main.js
+++ main.js
@@ -23,3 +23,66 @@
 
 
 
+// 3-masla
+
+// let nums = [5, 7, 7, 8, 8, 10]
+// let result = []
+// let target = 8
+// for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] == target) {
+//         result.push(i )
+//     }
+// }
+// if (result.length == 0) {
+//     console.log("Mavjud emas")
+// }
+// console.log(result)
+
+
+
+
+
+// 4-masala
+
+// let total = [3,8,4,1,78,45,92]
+// function findmax(param){
+//     let max = Math.max(...total)
+//     let min = Math.min(...total)
+//     console.log("max sonnidan " + (max - param) + " ta  kichik" );
+//     console.log("min sonnidan " + (min - param) + " ta  katta" );
+// }
+// findmax(45)
+
+
+
+
+
+
+// 5-masala
+
+
+// function findmax (number_1, number_2 ){
+    //     obj = {num_1: 1, num_2: 6, num_3: 3, num_4: 8 }
+    //     obj.num_1 = number_1
+    //     obj.num_4 = number_2
+    // }
+    // findmax(8, 1)
+    // console.log(obj);
+
+
+
+
+
+
+
+// 8-masala
+
+// let arr = [1, 3, 2.0, 23, 12];
+// let maxVal = Math.max(...arr);
+// let minVal = Math.min(...arr);
+
+// let maxIndex = arr.indexOf(maxVal);
+// let minIndex = arr.indexOf(minVal);
+// [arr[maxIndex], arr[minIndex]] = [arr[minIndex], arr[maxIndex]];
+
+// console.log("O'zgardi!:", arr); 
