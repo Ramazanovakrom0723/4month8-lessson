51139c1942ae6b80baa8a8bbedc4b26a866f78a8
Ramazanovakrom0723 <akromramazanov77@gmail.com>
2024-04-01 08:45:07 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+// 1-masala
+
+// let list1 = [1,2,4]
+// let list2 = [1,3,4] 
+// let union = [...list1,...list2]
+// console.log(union)
+// union.sort(function(a, b) {
+//     return a - b;
+//   })
+// console.log(union)  
+
+
+
+
+
+// 2-masala
+
+// let numbers = [4,5,6,7,0,1,2]
+// let index = numbers.indexOf(0)
+// console.log(index);
+
+
+
+
+
